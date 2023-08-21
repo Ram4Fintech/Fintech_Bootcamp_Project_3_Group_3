56c49dfb7025ae0a9ae5ace44d355cec0516a353
Ahura7 <125552102+Ahura7@users.noreply.github.com>
2023-08-21 20:37:33 +1000

    Updated Code with Appraise Function

--- Solidity_Code_Shell.sol
+++ Solidity_Code_Shell.sol
@@ -2,23 +2,19 @@ pragma solidity ^0.5.0;
 
 // Imports
 
-
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
 
 // Import another file from the same folder:
 import "./FileNmae.sol";
 
 
-/*----------------------------------------------------------------------------*/
-/*----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------*/
 
-// Token Contract Solidity Code to Mint NFTs
+// Token Contract Solidity Code to Mint NFTs Using ERC721
 
-//ERC721
-contract ArtToken is ERC721Full {
+
+contract SIR_NFT is ERC721Full {
     constructor() public ERC721Full("ArtToken", "ART") {}
 
     function registerArtwork(address owner, string memory tokenURI)
@@ -33,6 +29,69 @@ contract ArtToken is ERC721Full {
     }
 }
 
+contract ArtRegistry is ERC721Full {
+    constructor() public ERC721Full("ArtRegistryToken", "ART") {}
+
+    struct Artwork {
+        string name;
+        string artist;
+        uint256 appraisalValue;
+        string artJson;
+    }
+
+    mapping(uint256 => Artwork) public artCollection;
+
+    event Appraisal(uint256 tokenId, uint256 appraisalValue, string reportURI, string artJson);
+    
+    function imageUri(
+        uint256 tokenId
+
+    ) public view returns (string memory imageJson){
+        return artCollection[tokenId].artJson;
+    }
+
+
+    function registerArtwork(
+        address owner,
+        string memory name,
+        string memory artist,
+        uint256 initialAppraisalValue,
+        string memory tokenURI,
+        string memory tokenJSON
+    ) public returns (uint256) {
+        uint256 tokenId = totalSupply();
+
+        _mint(owner, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+
+        artCollection[tokenId] = Artwork(name, artist, initialAppraisalValue, tokenJSON);
+
+        return tokenId;
+    }
+
+    function newAppraisal(
+        uint256 tokenId,
+        uint256 newAppraisalValue,
+        string memory reportURI,
+        string memory tokenJSON
+        
+    ) public returns (uint256) {
+        artCollection[tokenId].appraisalValue = newAppraisalValue;
+
+        emit Appraisal(tokenId, newAppraisalValue, reportURI, tokenJSON);
+
+        return (artCollection[tokenId].appraisalValue);
+    }
+}
+
+
+/*-----------------------------------------------------------------------------*/
+/*-----------------------------------ARCHIVE-----------------------------------*/
+/*-----------------------------------------------------------------------------*/
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
 // ERC20
 contract XP_Token is ERC20, ERC20Detailed {
@@ -58,29 +117,6 @@ contract XP_Token is ERC20, ERC20Detailed {
         _mint(recipient, amount);
     }
 }
-
-/*----------------------------------------------------------------------------*/
-/*----------------------------------------------------------------------------*/
-
-// NFT Certificate Solidity Code
-
-contract certificate is ERC721Full {
-    constructor() public ERC721Full ("certificate","CERT") {}
-
-    function awardCertificate(address student, string memory tokenURI)
-        public
-        returns(uint256)
-        {
-            uint256 newCertificateId = totalSupply();
-            _mint(student, newCertificateId);
-            _setTokenURI(newCertificateId, tokenURI);
-
-            return newCertificateId;
-        }
-}
-
-/*----------------------------------------------------------------------------*/
-/*----------------------------------------------------------------------------*/
 
 // Transactions
 
