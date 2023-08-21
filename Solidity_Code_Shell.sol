4a159914be8da3b7dd42859779b4f49db3003beb
Ahura7 <125552102+Ahura7@users.noreply.github.com>
2023-08-21 12:52:24 +1000

    Updated shells with additional code sections

--- Solidity_Code_Shell.sol
+++ Solidity_Code_Shell.sol
@@ -15,7 +15,7 @@ import "./FileNmae.sol";
 /*----------------------------------------------------------------------------*/
 /*----------------------------------------------------------------------------*/
 
-// Token Contract Solidity Code
+// Token Contract Solidity Code to Mint NFTs
 
 //ERC721
 contract ArtToken is ERC721Full {
@@ -77,4 +77,118 @@ contract certificate is ERC721Full {
 
             return newCertificateId;
         }
+}
+
+/*----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
+
+// Transactions
+
+contract CustomerAccount {
+    address payable owner;
+    bool isNewAccount;
+    uint public accountBalance;
+    string customerName;
+    string customerLastName;
+    address payable authorizedRecipient;
+
+    function getInfo() view public returns(address, bool, uint, string memory, string memory) {
+        return (owner, isNewAccount, accountBalance, customerName, customerLastName);
+    }
+
+    function setInfo(address payable newOwner, bool newAccountStatus, uint newAccountBalance, string memory newCustomerName, string memory newCustomerLastName) public {
+        owner = newOwner;
+        isNewAccount = newAccountStatus;
+        accountBalance = newAccountBalance;
+        customerName = newCustomerName;
+        customerLastName = newCustomerLastName;
+    }
+
+    function sendRemittance(uint amount, address payable recipient) public {
+        require ((recipient == owner) || (recipient == authorizedRecipient), "Only the account owner and authorised recipient can transfer funds!");
+        recipient.transfer(amount);
+        accountBalance = address(this).balance;
+    }
+
+    function deposit() public payable {
+        accountBalance = address(this).balance;
+    }
+
+    function() external payable {}
+}
+
+// Define a new contract named `JointSavings`
+contract JointSavings {
+
+    /*
+    Inside the new contract define the following variables:
+    - Two variables of type `address payable` named `accountOne` and `accountTwo`
+    - A variable of type `address public` named `lastToWithdraw`
+    - Two variables of type `uint public` named `lastWithdrawAmount` and `contractBalance`.
+    */
+    address payable accountOne;
+    address payable accountTwo;
+    address public lastToWithdraw;
+    uint public lastWithdrawAmount;
+    uint public contractBalance;
+
+    /*
+    Define a function named **withdraw** that will accept two arguments.
+    - A `uint` variable named `amount`
+    - A `payable address` named `recipient`
+    */
+    function withdraw(uint amount, address payable recipient) public {
+
+        /*
+        Define a `require` statement that checks if the `recipient` is equal to either `accountOne` or `accountTwo`. The `requiere` statement returns the text `"You don't own this account!"` if it does not.
+        */
+        require((recipient == accountOne) || (recipient == accountTwo), "You don't own this account!");
+
+        /*
+        Define a `require` statement that checks if the `balance` is sufficient to accomplish the withdraw operation. If there are insufficient funds, the text `Insufficient funds!` is returned.
+        */
+        require(address(this).balance >= amount, "Insufficient funds!");
+
+        /*
+        Add and `if` statement to check if the `lastToWithdraw` is not equal to (`!=`) to `recipient` If `lastToWithdraw` is not equal, then set it to the current value of `recipient`.
+        */
+        if (lastToWithdraw != recipient) {
+            lastToWithdraw = recipient;
+        }
+
+        // Call the `transfer` function of the `recipient` and pass it the `amount` to transfer as an argument.
+        //return recipient.transfer(amount);  HAVE TO MOVE THIS BELOW TO RESOLVE ERROR/WARNING MESSAGE.
+
+        // Set  `lastWithdrawAmount` equal to `amount`
+        lastWithdrawAmount = amount;
+
+        // Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance` to reflect the new balance of the contract.
+        contractBalance = address(this).balance;
+
+        return recipient.transfer(amount);
+    }
+
+    // Define a `public payable` function named `deposit`.
+    function deposit() public payable {
+
+        /*
+        Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance`.
+        */
+        contractBalance = address(this).balance;
+    }
+
+    /*
+    Define a `public` function named `setAccounts` that receive two `address payable` arguments named `account1` and `account2`.
+    */
+    function setAccounts(address payable account1, address payable account2) public{
+
+        // Set the values of `accountOne` and `accountTwo` to `account1` and `account2` respectively.
+        accountOne = account1;
+        accountTwo = account2;
+    }
+
+    /*
+    Finally, add the **default fallback function** so that your contract can store Ether sent from outside the deposit function.
+    */
+    function() external payable {}
 }
